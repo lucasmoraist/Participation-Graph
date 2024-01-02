e775c5a64efe32abbfb6664894580fed1a4e9f8c
lucasmoraist <luksmn1101@gmail.com>
2024-01-02 13:23:53 -0300

    feat: add as rotas do servidor

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const routes = require("./routes")
 
 const app = express();
+
+routes(app);
 
 app.listen(3000, ()=>console.log("Servidor escutando na porta 3000"));
 
