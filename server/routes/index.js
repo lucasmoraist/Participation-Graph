16b283d8558514f5a24e713e327c6fc7fceb82de
lucasmoraist <luksmn1101@gmail.com>
2024-01-03 10:13:41 -0300

    feat: liberando cors policy

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,16 @@
 const bodyParser = require("body-parser");
+const cors = require('cors');
 
 const user = require("./UserRoute.js")
 
+const corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200
+};
+
 module.exports = app =>{
     app.use(
+        cors(corsOptions),
         bodyParser.json(),
         user
     )
