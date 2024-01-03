2f5aadf638af736077b23cb9373fe9aef95cb9db
lucasmoraist <luksmn1101@gmail.com>
2024-01-03 15:15:51 -0300

    feat: recebendo dados para serem passados para as tabelas

--- src/app/shared/main/main.component.ts
+++ src/app/shared/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-main',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  items: any[] = []
+
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.userService.getItems().subscribe(data => {
+      this.items = data;
+    })
   }
 
 }
