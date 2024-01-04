0f0f9a6fee8a1c0028076a1f2bbbd67d5d5c9dc7
lucasmoraist <luksmn1101@gmail.com>
2024-01-04 09:14:18 -0300

    feat: criando método paraenviar dados definidos através do obj para o bd

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+  }
+
+  obj = {
+    firstName: '',
+    lastName: '',
+    participation: ''
+  }
+
+  handleSubmit(){
+
+    console.log(this.obj);
+    
+    this.userService.postItems(this.obj).subscribe(response => {
+      console.log(response);
+    })
+
+    this.obj.firstName = ''
+    this.obj.lastName = ''
+    this.obj.participation = ''
   }
 
 }
